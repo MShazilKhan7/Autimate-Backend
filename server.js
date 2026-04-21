f6337271a6a8fda1115279053efa87312b8f44bc
Muhammad Shazil <shazilk14@gmail.com>
2026-04-21 17:12:31 +0500

    added url for CORS

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 import dotenv from "dotenv";
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 import { connectDB } from "./db/config.js";
 
-import AuthRoute from "./routes/Auth.Route.js"
+import AuthRoute from "./routes/Auth.Route.js";
 import UsersRoute from "./routes/Users.Route.js";
-import questionRoutes from './routes/Onboarding.Routes.js';
-import speechAceRoutes from './routes/SpeechAce.Routes.js';
-
+import questionRoutes from "./routes/Onboarding.Routes.js";
+import speechAceRoutes from "./routes/SpeechAce.Routes.js";
 
 dotenv.config();
 connectDB();
@@ -17,13 +16,18 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
-app.use(cors({
-  origin: ["http://localhost:3000", "http://localhost:8080"],
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-}));
-
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:8080",
+      "https://autimateapp.vercel.app",
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
 // Routers
 app.get("/", (req, res) => {
@@ -34,8 +38,8 @@ app.get("/api/version", (req, res) => {
 });
 app.use("/api/auth", AuthRoute);
 app.use("/api/users", UsersRoute);
-app.use('/api/on-boarding', questionRoutes)
-app.use('/api/score-speech', speechAceRoutes)
+app.use("/api/on-boarding", questionRoutes);
+app.use("/api/score-speech", speechAceRoutes);
 
 const port = process.env.PORT || 5000;
 
