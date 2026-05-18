87ccb6abb87ff7111b73b4a9f74c35af24d6b38a
MuhammadShazilKhan <140618032+MShazilKhan7@users.noreply.github.com>
2026-05-18 11:38:46 +0500

    Update server.js

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ app.use(
       "http://localhost:8080",
       "http://localhost:8081",
       "https://autimateapp.vercel.app",
+      "https://autimateadmin.vercel.app"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
